9825d9af938da6efa833ed96960482f1268566d4
José Matheus R. Flora <jmrfloraxbox@gmail.com>
2023-11-13 10:47:02 -0300

    troca funcionando

--- bibliotecaGUI/app/main.py
+++ bibliotecaGUI/app/main.py
@@ -30,19 +30,31 @@ class App(ctk.CTk):
         # ir adicionado frames aqui:
         for frame in (FrameLoginContainer, teste):
             self.frames[frame] = frame(self.container)
-                
-        # self.selecionar_frame_container(FrameLoginContainer)     
+            self.frames[frame].grid(row=0, column=0, sticky= "nsew")
+        
+        # teste de troca de frames    
+        button = ctk.CTkButton( master= self,text="teste", command= lambda: self.selecionar_frame_container(teste))
+        button.place(relx=0.1, rely=0.85)
+        
+        button2 = ctk.CTkButton( master= self,text="frame", command= lambda: self.selecionar_frame_container(FrameLoginContainer))
+        button2.place(relx=0.3, rely=0.85)
+        
     
     def selecionar_frame_container(self, frame: ctk.CTkFrame):
-        if self.current_frame:
-            self.current_frame.grid_forget()  # Forget the current frame
+        # if self.current_frame:
+        #     self.current_frame.grid_forget()  # Forget the current frame
         
-        self.current_frame = self.frames[frame]
-        self.current_frame.grid(row=0, column=0, sticky="nsew")  # Display the new frame
+        # self.current_frame = self.frames[frame]
+        
+        # self.current_frame.grid(row=0, column=0, sticky="nsew")  # Display the new frame
 
-        # Additional configuration to ensure the frame displays properly
-        self.current_frame.tkraise()
-        self.current_frame.update_idletasks()
+        # # Additional configuration to ensure the frame displays properly
+        # self.current_frame.tkraise()
+        # self.current_frame.update_idletasks()
+        
+        framevar: ctk.CTkFrame = self.frames[frame]
+        
+        framevar.tkraise()
 
     
         
@@ -108,13 +120,15 @@ class EntryLoginFrame(ctk.CTkFrame):
 
 class teste(ctk.CTkFrame):
     def __init__(self, parente):
-        super().__init__(master=parente, fg_color="transparent")
+        super().__init__(master=parente)
         
 
 if __name__ == '__main__':
     app = App()
     # app.selecionar_frame_container(FrameLoginContainer)
     
+    app.selecionar_frame_container(FrameLoginContainer)
+    app.selecionar_frame_container(teste)
     app.selecionar_frame_container(FrameLoginContainer)
     app.mainloop()  # Keep mainloop at the end of your application's setup
     
